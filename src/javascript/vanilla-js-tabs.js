8b385584f8f32343325b3e95d8135d3ae6a28523
Zoltan Toth <ztoth@indigo.ca>
2017-08-23 18:29:10 -0400

    script, styles and page setup

---
+++ src/javascript/vanilla-js-tabs.js
@@ -0,0 +1,95 @@
+/**
+* @fileOverview
+* @author Zoltan Toth
+* @version 1.0.0
+*/
+
+/**
+* @description
+* Vanilla Javascript Tabs
+*
+* @class
+* @param {string} options.elem - The HTML id of the tabs container.
+* @param {number} [options.openTab = 0] - Render the tabs with this item open.
+* @param {string} [options.titleClass = "js-tabs__title"] - CSS class of the tab titles.
+* @param {string} [options.activeClass = "js-tabs__title_active"] - CSS class of the active title.
+* @param {string} [options.contentClass = "js-tabs__content"] - CSS class of the tab content.
+*/
+var Tabs = function(options) {
+    var elem         = document.getElementById(options.elem),
+        openTab      = options.open || 0,
+        titleClass   = options.titleClass || 'js-tabs__title',
+        activeClass  = options.activeClass || 'js-tabs__title_active',
+        contentClass = options.contentClass || 'js-tabs__content',
+        tabsNum      = elem.querySelectorAll('.' + titleClass).length;
+        
+    render();
+    
+    /**
+     * Initial rendering of the tabs.
+     */
+    function render() {
+        elem.addEventListener('click', onClick);
+        
+        var init = (openTab < 0 || isNaN(openTab) || openTab > tabsNum) ? 0 : openTab;
+  
+        for (var i = 0; i < tabsNum; i++) {
+            elem.querySelectorAll('.' + titleClass)[i].setAttribute('data-index', i);
+            if (i === init) open(i);
+        }
+    }
+
+    /**
+     * Handle clicks on the tabs.
+     * 
+     * @param {object} e - Element the click occured on.
+     */
+    function onClick(e) {
+        if (e.target.className.indexOf(titleClass) === -1) return;
+        e.preventDefault();
+
+        open(e.target.getAttribute('data-index'));
+    }
+    
+    /**
+     * Hide all tabs and re-set tab titles.
+     */
+    function reset() {
+        [].forEach.call(elem.querySelectorAll('.' + contentClass), function(item) {
+            item.style.display = 'none';
+        });
+        
+        [].forEach.call(elem.querySelectorAll('.' + titleClass), function(item) {
+            item.className = removeClass(item.className, activeClass);
+        });
+    }
+    
+    /**
+    * Utility function to remove the open class from tab titles.
+    *
+    * @param {string} str - Current class.
+    * @param {string} cls - The class to remove.
+    */
+    function removeClass(str, cls) {
+        var reg = new RegExp('(\ )' + cls + '(\)', 'g');
+        return str.replace(reg, '');
+    }
+    
+    /**
+     * Opens a tab by index.
+     * 
+     * @param {number} n - Index of tab to open. Starts at 0.
+     * 
+     * @public
+     */
+    function open(n) {
+        reset();
+
+        elem.querySelectorAll('.' + titleClass)[n].className += ' ' + activeClass;
+        elem.querySelectorAll('.' + contentClass)[n].style.display = '';
+    }
+
+    return {
+        open: open
+    };
+};
